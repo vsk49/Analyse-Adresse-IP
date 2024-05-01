00836fc04577028fbda79780fc5b55343576a02c
Koh Virgil-Shaun <virgil-shaun.koh@etu.iut-tlse3.fr>
2024-05-01 22:45:58 +0200

    setup of structs (ou enregistrements)

---
+++ structs.h
@@ -0,0 +1,45 @@
+/******************************************************************************
+*  ASR => 4R2.04                                                              *
+*******************************************************************************
+*                                                                             *
+*  N° de Sujet :  2                                                           *
+*                                                                             *
+*******************************************************************************
+*                                                                             *
+*  Intitulé :    Analyse Adresse IP                                           *
+*                                                                             *
+*******************************************************************************
+*                                                                             *
+*  Nom-prénom1 :     Koh-Virgil Shaun                                         *
+*                                                                             *
+*  Nom-prénom2 :     Lfarh-Mouataz                                            *
+*                                                                             *
+*******************************************************************************
+*                                                                             *
+*  Nom du fichier :    structs.h                                              *
+*                                                                             *
+******************************************************************************/
+
+#ifndef _STRUCTS
+
+    #define _STRUCTS
+    #include "constantes.h"
+
+    typedef struct AdresseChaine {
+        char* octets[NB_BYTES];
+        char* mask;
+    };
+    
+    typedef struct AdresseDecimale {
+        int octets[NB_BYTES];
+        int mask;
+    };
+
+    typedef struct AdressesAAfficher {
+        char adresseClasse;
+        char adresseType[MAX_LENGTH];
+        int adresseReseau[NB_BYTES];
+        int adresseMachineHote[NB_BYTES];
+    };
+
+#endif
